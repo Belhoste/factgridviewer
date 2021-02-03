1b31e7c2787052bf29747664843e13a6e21025c3
Bruno BELHOSTE <bbelhoste@gmail.com>
2021-02-03 15:06:50 +0100

    toujours CORS

--- server.js
+++ server.js
@@ -3,26 +3,12 @@ const cors = require('cors');
 
 const app = express();
 
-const whitelist = ['http://localhost:3000']; // list of allow domain
+app.use(cors());
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
+app.options('*', cors())
 
 app.use(express.static('./dist/factgrid'));
 
-app.use(cors(corsOptions));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/factgrid/'}),
